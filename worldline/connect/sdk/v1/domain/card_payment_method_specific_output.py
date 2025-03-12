68fa2f9e140247e670a9c8b9fda52ea7dcc7346d
WorldlineConnect <github.connect@worldline.com>
2025-03-12 15:07:55 +0100

    Release 4.4.0.

--- worldline/connect/sdk/v1/domain/card_payment_method_specific_output.py
+++ worldline/connect/sdk/v1/domain/card_payment_method_specific_output.py
@@ -6,6 +6,7 @@
 from worldline.connect.sdk.v1.domain.abstract_payment_method_specific_output import AbstractPaymentMethodSpecificOutput
 from worldline.connect.sdk.v1.domain.card_essentials import CardEssentials
 from worldline.connect.sdk.v1.domain.card_fraud_results import CardFraudResults
+from worldline.connect.sdk.v1.domain.network_token_data import NetworkTokenData
 from worldline.connect.sdk.v1.domain.three_d_secure_results import ThreeDSecureResults
 
 
@@ -18,7 +19,9 @@ class CardPaymentMethodSpecificOutput(AbstractPaymentMethodSpecificOutput):
     __card = None
     __fraud_results = None
     __initial_scheme_transaction_id = None
+    __network_token_data = None
     __network_token_used = None
+    __payment_account_reference = None
     __scheme_transaction_id = None
     __three_d_secure_results = None
     __token = None
@@ -77,6 +80,19 @@ class CardPaymentMethodSpecificOutput(AbstractPaymentMethodSpecificOutput):
         self.__initial_scheme_transaction_id = value
 
     @property
+    def network_token_data(self):
+        """
+        | Object holding data that describes a network token.
+
+        Type: :class:`worldline.connect.sdk.v1.domain.network_token_data.NetworkTokenData`
+        """
+        return self.__network_token_data
+
+    @network_token_data.setter
+    def network_token_data(self, value):
+        self.__network_token_data = value
+
+    @property
     def network_token_used(self):
         """
         | Indicates if a network token was used during the payment.
@@ -88,6 +104,19 @@ class CardPaymentMethodSpecificOutput(AbstractPaymentMethodSpecificOutput):
     @network_token_used.setter
     def network_token_used(self, value):
         self.__network_token_used = value
+
+    @property
+    def payment_account_reference(self):
+        """
+        | A unique reference to the primary account number. Payment Account Reference provides a consolidated view of transactions associated with a PAN and its affiliated tokens, making it easier to identify customers and their associated transactions across payment channels.
+
+        Type: str
+        """
+        return self.__payment_account_reference
+
+    @payment_account_reference.setter
+    def payment_account_reference(self, value):
+        self.__payment_account_reference = value
 
     @property
     def scheme_transaction_id(self):
@@ -139,8 +168,12 @@ class CardPaymentMethodSpecificOutput(AbstractPaymentMethodSpecificOutput):
             dictionary['fraudResults'] = self.fraud_results.to_dictionary()
         if self.initial_scheme_transaction_id is not None:
             dictionary['initialSchemeTransactionId'] = self.initial_scheme_transaction_id
+        if self.network_token_data is not None:
+            dictionary['networkTokenData'] = self.network_token_data.to_dictionary()
         if self.network_token_used is not None:
             dictionary['networkTokenUsed'] = self.network_token_used
+        if self.payment_account_reference is not None:
+            dictionary['paymentAccountReference'] = self.payment_account_reference
         if self.scheme_transaction_id is not None:
             dictionary['schemeTransactionId'] = self.scheme_transaction_id
         if self.three_d_secure_results is not None:
@@ -165,8 +198,15 @@ class CardPaymentMethodSpecificOutput(AbstractPaymentMethodSpecificOutput):
             self.fraud_results = value.from_dictionary(dictionary['fraudResults'])
         if 'initialSchemeTransactionId' in dictionary:
             self.initial_scheme_transaction_id = dictionary['initialSchemeTransactionId']
+        if 'networkTokenData' in dictionary:
+            if not isinstance(dictionary['networkTokenData'], dict):
+                raise TypeError('value \'{}\' is not a dictionary'.format(dictionary['networkTokenData']))
+            value = NetworkTokenData()
+            self.network_token_data = value.from_dictionary(dictionary['networkTokenData'])
         if 'networkTokenUsed' in dictionary:
             self.network_token_used = dictionary['networkTokenUsed']
+        if 'paymentAccountReference' in dictionary:
+            self.payment_account_reference = dictionary['paymentAccountReference']
         if 'schemeTransactionId' in dictionary:
             self.scheme_transaction_id = dictionary['schemeTransactionId']
         if 'threeDSecureResults' in dictionary:
